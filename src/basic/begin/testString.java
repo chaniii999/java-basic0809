966a69bd203409e3db4a5969cf1fdfdcf00773fe
chani999 <psc0729@naver.com>
2024-08-12 09:27:17 +0900

    day2 - String_Exercise

---
+++ src/basic/begin/testString.java
@@ -0,0 +1,12 @@
+package basic.begin;
+
+public class testString {
+    public static void main(String[] args) {
+        String str = "hello";
+
+        String s1 = "my dream";
+        String s2 = " is a programmer";
+
+        System.out.println(s1+s2);
+    }
+}
