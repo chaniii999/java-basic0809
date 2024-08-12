4c12e8f47ba1dcb251e1efda516f9dc243472bef
chani999 <psc0729@naver.com>
2024-08-12 12:00:57 +0900

    day2 - 여러가지 변경사항~

---
+++ src/basic/operator/AssignmentOperator.java
@@ -0,0 +1,8 @@
+package basic.operator;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+//        복합 대입 연산자 ( +=, -=, *= )
+
+    }
+}
---
+++ src/basic/operator/UnaryOperator.java
@@ -0,0 +1,12 @@
+package basic.operator;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        //증감 연산자 ( ++, --)
+
+        int x = 1;
+        int y = x++;
+        int z = x--;
+
+    }
+}
