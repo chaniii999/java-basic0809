a8f5a6c31068f1cc8379bc6c8dfd42aab1d96ca3
chani999 <psc0729@naver.com>
2024-08-12 17:49:37 +0900

    day2 - Loop Exercise

---
+++ src/basic/constate/LoopNesting2.java
@@ -0,0 +1,14 @@
+package basic.constate;
+
+public class LoopNesting2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i++) {
+            for (int k = 4-i; k > 0; k--)
+                System.out.print(" ");
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
