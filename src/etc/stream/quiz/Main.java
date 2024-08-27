fbbb015016f596de0894ad451cc7cee02b81262b
chani999 <psc0729@naver.com>
2024-08-27 10:06:09 +0900

    0827 - Stream Quiz ++

--- src/etc/stream/quiz/Main.java
+++ src/etc/stream/quiz/Main.java
@@ -2,6 +2,10 @@ package etc.stream.quiz;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 
 public class Main {
@@ -33,7 +37,7 @@ public class Main {
         // 연습 2: 거래자가 근무하는 모든 도시이름을 중복 없이 나열하시오.
         transactions.stream()
                 .map(t -> t.getTrader().getCity())
-                .toList()
+                .collect(Collectors.toSet())
                 .forEach(System.out::println);
 
         System.out.println("Cambridge에 근무하는 모든 거래자(Trader)를 찾아");
@@ -58,7 +62,7 @@ public class Main {
         System.out.println("Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?");
         // 연습 5: Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?
         boolean flag = transactions.stream()
-                        .anyMatch(t -> t.getTrader().getCity().equals("Milan"));
+                .anyMatch(t -> t.getTrader().getCity().equals("Milan"));
         System.out.println("flag = " + flag);
 
         System.out.println("연습 6: Cambridge에 사는 거래자의 모든 거래액의 총합 출력.");
@@ -71,12 +75,14 @@ public class Main {
         // 연습 7: 모든 거래에서 최고거래액은 얼마인가?
         int max = transactions.stream()
                 .mapToInt(Transaction::getValue)
-                .max().getAsInt();
+                .max()
+                .getAsInt();
         System.out.println("max = " + max);
         // 연습 8. 가장 작은 거래액을 가진 거래정보 탐색
         int min = transactions.stream()
                 .mapToInt(Transaction::getValue)
-                .min().getAsInt();
+                .min()
+                .getAsInt();
         System.out.println("min = " + min);
 
         System.out.println("연습 9. 거래액이 500 이상인 거래들만 필터링하고,");
@@ -96,6 +102,25 @@ public class Main {
                 .mapToInt(t -> t)
                 .average().getAsDouble();
         System.out.println("avg = " + avg);
+
+        System.out.println("연습 11번: cambridge 에서 거래하는 모든 거래자의 거래정보를 연도별로 그룹화하라");
+
+        transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(t -> t.getYear()))
+                .toList()
+                .forEach(System.out::println);
+
+        Map<Integer, List<Transaction>> cambridge = transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .collect(groupingBy(Transaction::getYear));
+
+        System.out.println("==========================");
+
+        cambridge.forEach((key, value) -> {
+            System.out.println("year = " + key);
+            value.forEach(System.out::println);
+        });
 
 
     }
